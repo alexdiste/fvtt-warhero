1fcdcf435ae06d78ac5a48a0bd258d245b3b1259
Alessandro Di Stefano <alex_diste@hotmail.it>
2025-06-07 05:16:13 +0200

    mod

--- modules/warhero-item.js
+++ modules/warhero-item.js
@@ -2,21 +2,21 @@ import { WarheroUtility } from "./warhero-utility.js";
 
 export const defaultItemImg = {
   equipment: "systems/fvtt-warhero/images/icons/swap-bag.svg",
-  race: "systems/fvtt-warhero/images/icons/razze.webp",
-  weapon: "systems/fvtt-warhero/images/icons/two-handed-sword.svg",
-  armor: "systems/fvtt-warhero/images/icons/difesa.webp",
-  shield: "systems/fvtt-warhero/images/icons/difensiva.webp",
-  skill: "systems/fvtt-warhero/images/icons/talenti.webp",  
-  power: "systems/fvtt-warhero/images/icons/magia.webp",
-  language: "systems/fvtt-warhero/images/icons/linguaggi.webp",
-  condition: "systems/fvtt-warhero/images/icons/stordenti.webp",  
-  class: "systems/fvtt-warhero/images/icons/classe.webp",
+  race: "systems/fvtt-warhero/images/icons/dwarf-face.svg",
+  weapon: "systems/fvtt-warhero/images/icons/crossed-swords.svg",
+  armor: "systems/fvtt-warhero/images/icons/abdominal-armor.svg",
+  shield: "systems/fvtt-warhero/images/icons/shield.svg",
+  skill: "systems/fvtt-warhero/images/icons/deadly-strike.svg",  
+  power: "systems/fvtt-warhero/images/icons/burning-meteor.svg",
+  language: "systems/fvtt-warhero/images/icons/spiked-halo.svg",
+  condition: "systems/fvtt-warhero/images/icons/aura.svg",  
+  class: "systems/fvtt-warhero/images/icons/elf-helmet.svg",
   money: "systems/fvtt-warhero/images/icons/two-coins.svg",
-  potion: "systems/fvtt-warhero/images/icons/swap-bag.svg",
-  poison: "systems/fvtt-warhero/images/icons/swap-bag.svg",
+  potion: "systems/fvtt-warhero/images/icons/potion-ball.svg",
+  poison: "systems/fvtt-warhero/images/icons/vial.svg",
   trap: "systems/fvtt-warhero/images/icons/swap-bag.svg",
   classitem: "systems/fvtt-warhero/images/icons/swap-bag.svg",
-  competency: "systems/fvtt-warhero/images/icons/swap-bag.svg"  
+  competency: "systems/fvtt-warhero/images/icons/battle-gear.svg"  
 }
 
 /**
@@ -31,5 +31,4 @@ export class WarheroItem extends Item {
     }
     super(data, context);
   }
-
 }
